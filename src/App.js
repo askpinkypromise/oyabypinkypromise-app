e034e8a8a1aa4284ae4e964d473b2f36dfa7abf3
vyasakanksha <vyasakanksha@gmail.com>
2023-10-20 02:38:10 +0530

    added media content

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import NavBar from "./components/NavBar";
 import ChatBox from "./components/ChatBox";
 import HomePage from "./components/HomePage";
 import Welcome from "./components/Welcome";
-import { BrowserRouter as Router,Routes, Switch, Route, Link } from 'react-router-dom'; 
+import MediaContent from "./components/MediaContent";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   // const [user] = useAuthState(auth);
 
   return (
-  <div className="App">
-  //     {/* <NavBar />
+    <div className="App">
+      //{" "}
+      {/* <NavBar />
   //     {!user ? (
   //       <Welcome />
   //     ) : (
@@ -20,14 +29,15 @@ function App() {
   //         <HomePage />
   //       </>
   //     )} */}
-    {/* <NavBar/> */}
-      <Router> 
+      {/* <NavBar/> */}
+      <Router>
         <Routes>
-          <Route exact path='/' element={<Welcome/>}></Route> 
-          <Route exact path='/home' element={<HomePage/>}></Route> 
-          <Route exact path='/chat' element={<ChatBox/>}></Route> 
+          <Route exact path="/" element={<Welcome />}></Route>
+          <Route exact path="/home" element={<HomePage />}></Route>
+          <Route exact path="/chat" element={<ChatBox />}></Route>
+          <Route exact path="/mediacontent" element={<MediaContent />}></Route>
         </Routes>
-      </Router> 
+      </Router>
     </div>
   );
 }
---
+++ src/components/MediaContent.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+
+const FirestoreData = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch data from Firestore
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "content")); // Replace with your Firestore collection name
+        const documents = [];
+        querySnapshot.forEach((doc) => {
+          documents.push({ id: doc.id, ...doc.data() });
+        });
+        setData(documents);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // Call the fetchData function when the component mounts
+    fetchData();
+  }, []); // Empty dependency array ensures this effect runs once on mount
+
+  return (
+    <div>
+      <h1>Content</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <div>
+              {/* Assuming the link field contains YouTube video URLs */}
+              <iframe
+                title={item.title}
+                width="560"
+                height="315"
+                src={item.link}
+                frameborder="0"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FirestoreData;
